c597dc826a2261e6ad4866666613762fb7dd4462
jinaluvani <46022527+jinaluvani@users.noreply.github.com>
2025-04-02 10:34:54 +0530

    task list added

---
+++ tasks.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+require_once 'classes/Task.php';
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$task = new Task();
+$user_id = $_SESSION['user_id'];
+$isAdmin = ($_SESSION['role'] === 'admin');
+
+
+$tasks = $task->getTasks($user_id, $isAdmin);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task Manager</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<h2>My Tasks</h2>
+
+
+<table border="1">
+    <tr>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Due Date</th>
+        <th>Priority</th>
+        <th>Status</th>
+        <th>Actions</th>
+    </tr>
+    <?php foreach ($tasks as $task): ?>
+    <tr>
+        <td><?= htmlspecialchars($task['title']) ?></td>
+        <td><?= htmlspecialchars($task['description']) ?></td>
+        <td><?= htmlspecialchars($task['due_date']) ?></td>
+        <td><?= htmlspecialchars($task['priority']) ?></td>
+        <td id = "status"><?= htmlspecialchars($task['status']) ?></td>
+        <td>
+            <a href="edit_task.php?id=<?= $task['id'] ?>" id="edit-option">Edit</a> | 
+            <a href="delete_task.php?id=<?= $task['id'] ?>" onclick="return confirm('Are you sure?')">Delete</a>
+            <?php if ($task['status'] !== 'Completed'): ?>
+                <button class="complete-task" data-id="<?= $task['id'] ?>">Mark as Completed</button>
+            <?php else: ?>
+                <span></span>
+            <?php endif; ?>
+        </td>
+    </tr>
+    <?php endforeach; ?>
+</table>
+
+
+</body>
+</html>
